e71a1cb58ab59813abb3646ce5b2aee035bb409a
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2017-12-28 22:20:30 +0530

    User can create,delete,edit polls but can't add options

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -18,6 +18,30 @@ class PollsController < ApplicationController
     end
   end
 
+  def edit
+    @poll = Poll.find_by_id(params[:id])
+  end
+
+  def update
+    @poll = Poll.find_by_id(params[:id])
+    if @poll.update_attributes(poll_params)
+      flash[:success] = 'Poll was updated!'
+      redirect_to polls_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @poll = Poll.find_by_id(params[:id])
+    if @poll.destroy
+      flash[:success] = 'Poll was destroyed!'
+    else
+      flash[:warning] = 'Error destroying poll...'
+    end
+    redirect_to polls_path
+  end
+
   private
 
   def poll_params

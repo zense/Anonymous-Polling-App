d2e70f08b23f9abfd5be21490e64e98db3c54c48
Atibhi Agrawal <Atibhi.Agrawal@iiitb.org>
2018-04-17 00:30:05 +0530

    Oauth fixed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1000940372550-vro2o2evede3cjmjm9ni39bls23hn3m9.apps.googleusercontent.com', 'Fa7i3eSuTjpropiuuYPnn2PC', { :skip_jwt => true }
+  provider :google_oauth2, '210285683926-180cmt6demnastaked3llabm2ro5uf2g.apps.googleusercontent.com', 'RHYzR-VAF_yzngpzCsUMQOdt', { :skip_jwt => true }
 end
